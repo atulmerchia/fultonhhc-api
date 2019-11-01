de10d854ca0ae12885a65466eaace2cd4d0dbfab
Atul Merchia <atulmerchia@gmail.com>
2019-10-31 22:36:42 -0400

    resolve path issues

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
+const path = require('path');
+
 const PORT = process.env.PORT || 8000;
 const express = require('express');
 const app = express();
-const firebaseConfig = require(`./config/firebase-${process.env.DB_MODE}.json`);
+const firebaseConfig = require(path.resolve(`config/firebase-${process.env.DB_MODE}.json`));
 const Firebase = require('firebase').initializeApp(firebaseConfig);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}!`));
