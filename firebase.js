df521f1c0c5992334e5a6b9e53134da0c34ee63e
Atul Merchia <atulmerchia@gmail.com>
2019-11-19 15:53:14 -0500

    scp rollback

--- firebase.js
+++ firebase.js
@@ -1,8 +1,7 @@
-// const FIREBASE_KEYS = ["apiKey", "authDomain", "databaseURL", "projectId", "storageBucket", "messagingSenderId", "appId"];
-// const firebaseConfig = process.env.IS_LOCAL
-//   ? require(`./config/firebase-${process.env.DB_MODE}.json`)
-//   : FIREBASE_KEYS.reduce((acc, key) => Object.assign(acc, { [key]: process.env[key] }), {});
-const firebaseConfig = require(`./config/firebase-dev.json`)
+const FIREBASE_KEYS = ["apiKey", "authDomain", "databaseURL", "projectId", "storageBucket", "messagingSenderId", "appId"];
+const firebaseConfig = process.env.IS_LOCAL
+  ? require(`./config/firebase-${process.env.DB_MODE}.json`)
+  : FIREBASE_KEYS.reduce((acc, key) => Object.assign(acc, { [key]: process.env[key] }), {});
 
 const FirebaseApp = require('firebase').initializeApp(firebaseConfig);
 const uuid = require('uuid/v4')
